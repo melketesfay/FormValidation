65109f16d2f7d6664787bf4b4d4b6aed11320e2a
melketesfay <melketesfay@gmail.com>
2024-01-25 12:08:48 +0100

    fixed all Trello issues

--- script.js
+++ script.js
@@ -1,59 +1,34 @@
 function checkInput() {
   event.preventDefault();
+
+  //Select all input fields
   let data = document.querySelectorAll(".inputGroup input");
   let arrData = Array.from(data);
   let checkAGB = document.getElementById("agb").checked;
-  // console.log(!checkAGB);
+
+  //Set SessionStorage for Vorname and Nachname
   sessionStorage.setItem("vorname", data[0].value);
   sessionStorage.setItem("nachname", data[1].value);
 
   let wrongData = [];
-  let goodData = [];
-
-  for (let i = 0; i < data.length; i++) {
-    if (data[i].value !== "") {
-      goodData.push(data[i]);
-    }
-  }
-
+ 
+//Fill wrongData with all inputfields that are empty
   for (let i = 0; i < data.length; i++) {
     if (data[i].value == "") {
       wrongData.push(data[i]);
     }
   }
-  //   data.forEach((e) => (e.style.border = "none"));
 
+
+  //remove error messages that has been created from the previous try
   document.querySelectorAll("p").forEach((e) => e.remove());
 
-  document.querySelectorAll("p").forEach((e) => (e.id = "alert"));
 
+
+  //for all empty fields call the function errorMessages and show the error message
   if (wrongData.length > 0 || !checkAGB.checked) {
     if (wrongData.length > 0) {
-      wrongData.forEach((e) => {
-        let alertnode = document.createElement("p");
-
-        alertnode.style.cssText = 
-                `
-                margin-bottom: 5px;
-                margin-top:0px;
-                padding-left:5px;
-                color:#bb0000;
-                font-weight:500;
-                display:block;
-                border: 2px solid red;
-                border-radius:5px;
-                height:25px;
-                `;
-  
-       
-        alertnode.appendChild(
-          document.createTextNode(`Bitte ${e.name} eingeben`)
-        );
-        e.parentNode.insertBefore(alertnode, e);
-
-   
-      });
-
+      errorMessages(wrongData);
       return false;
     } else if (!checkAGB) {
       alert("Please Accept Terms");
@@ -85,4 +60,40 @@ function removeWarning() {
       errorMsg.remove();
     }, 500);
   }
+}
+
+
+
+
+//shows error message if input is incorrect
+function errorMessages(arr){
+  let wrongData = arr;
+
+  wrongData.forEach((e) => {
+   
+    let alertnode = document.createElement("p");
+
+    alertnode.style.cssText = 
+            `
+            margin-bottom: 5px;
+            margin-top:0px;
+            padding-left:5px;
+            color:#bb0000;
+            font-weight:500;
+            display:block;
+            border: 2px solid red;
+            border-radius:5px;
+            height:25px;
+            `;
+  
+   
+    alertnode.appendChild(
+      document.createTextNode(`Bitte ${e.name} eingeben`)
+    );
+    e.parentNode.insertBefore(alertnode, e);
+
+  });
+
+  return false;
+
 }
